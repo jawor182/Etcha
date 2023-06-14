55c9421909263503b7b7748b611940317f56256d
jawor182 <kubajaworski59@outlook.com>
2023-06-14 17:27:01 +0200

    fixed hover bug and now u can pick a color

--- scirpt.js
+++ scirpt.js
@@ -1,22 +1,49 @@
-const container = document.querySelector('.container');
-const stylecss = '.cell:hover{background-color: white;}';
-for (let i = 0; i < 576; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.id = i;
-    
-    cell.addEventListener('click', () =>  {
-        
-        cell.style.backgroundColor = 'black';
-        
-        
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const colorGetter = document.getElementById('colorGetter');
+    const resetBtn = document.querySelector('.resetBtn');
+  
+    let currentColor = 'black';
+  
+    for (let i = 0; i < 576; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.id = i;
+  
+      cell.addEventListener('click', () => {
+        cell.style.backgroundColor = currentColor;
+      });
+  
+      container.appendChild(cell);
+    }
+  
+    const updateDynamicStyle = () => {
+      const stylecss = `.cell:hover { background-color: ${currentColor}; }`;
+      const styleElement = document.getElementById('dynamicStyle');
+  
+      if (styleElement) {
+        styleElement.textContent = stylecss;
+      } else {
+        const newStyle = document.createElement('style');
+        newStyle.id = 'dynamicStyle';
+        newStyle.textContent = stylecss;
+        document.head.appendChild(newStyle);
+      }
+    };
+  
+    resetBtn.addEventListener('click', () => {
+      const cells = document.querySelectorAll('.cell');
+      cells.forEach(cell => {
+        cell.style.backgroundColor = 'white';
+        cell.style =`.cell:hover { background-color: ${currentColor}; }`;
+      });
     });
-    const button = document.querySelector('.resetBtn');
-    button.addEventListener('click', () => {
-            cell.style.backgroundColor = 'white';
-            
-            
-        });
-    container.appendChild(cell);
-
-}
+  
+    colorGetter.addEventListener('input', () => {
+      currentColor = colorGetter.value;
+      updateDynamicStyle();
+    });
+  
+    updateDynamicStyle();
+  });
+  
